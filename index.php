434af1b19ab61f1554a40744ce01f4c3b6e21283
Daniel Stanfield <daniel@dscreative.com.au>
2013-12-03 09:20:45 +1000

    Initial

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+		<title>Country Junction - Site Under Construction</title>
+		<style>
+		body {
+			background-color: #FAE1CD;
+			font-size: 14pt;
+			font-family: Arial;
+			margin: 0;
+			padding: 0;
+		}
+		a {
+			color: #000;
+		}
+		#notice {
+			margin: 200px auto 0;
+		}
+		.title {
+			font-size: 22pt;
+		}
+		</style>
+	</head>
+	<body>
+	<center id="notice">
+		<span class="title">Country Junction</span><br>Site Under Construction..<br><br>
+		If you&rsquo;re interested in our fortified beef or top quality Dorper lamb<br>send us an email to <a href="mailto:contact@countryjunction.com.au">contact@countryjunction.com.au</a> or call on 0410 120 200
+	</center>
+	</body>
+</html>
